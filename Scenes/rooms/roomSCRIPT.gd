90af4ff70bab4d01a1fc780fbf64a6da99b76570
deepdass <deepdas26052008@gmail.com>
2025-09-24 19:23:12 +0530

    idk

--- Scenes/rooms/roomSCRIPT.gd
+++ Scenes/rooms/roomSCRIPT.gd
@@ -1,4 +1,7 @@
-extends Node3D
+extends Node
+
+@export var grid_map_node: GridMap
+@export var parent_for_meshes: Node3D # Where the new MeshInstance3D nodes will be added
 
 
 var power_up_Chest : PackedScene = load("res://Scenes/powerUPs/PowerUp_chest.tscn")
@@ -17,6 +20,38 @@ var num_enemyCount : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if grid_map_node and parent_for_meshes:
+		convert_gridmap_to_meshes()
+
+
+func convert_gridmap_to_meshes():
+	if not grid_map_node.mesh_library:
+		print("GridMap has no MeshLibrary assigned.")
+		return
+
+	var mesh_library = grid_map_node.mesh_library
+	var cell_size = grid_map_node.cell_size
+
+	for x in range(grid_map_node.get_used_rect().position.x, grid_map_node.get_used_rect().end.x):
+		for y in range(grid_map_node.get_used_rect().position.y, grid_map_node.get_used_rect().end.y):
+			for z in range(grid_map_node.get_used_rect().position.z, grid_map_node.get_used_rect().end.z):
+				var item_index = grid_map_node.get_cell_item(x, y, z)
+				if item_index != GridMap.INVALID_CELL_ITEM:
+					var mesh = mesh_library.get_item_mesh(item_index)
+					if mesh:
+						var new_mesh_instance = MeshInstance3D.new()
+						new_mesh_instance.mesh = mesh
+
+						var cell_world_pos = grid_map_node.map_to_world(Vector3i(x, y, z))
+						new_mesh_instance.global_transform.origin = cell_world_pos
+
+						var orientation_index = grid_map_node.get_cell_item_orientation(x, y, z)
+						var rotation_basis = grid_map_node.get_basis_from_orientation(orientation_index)
+						new_mesh_instance.global_transform.basis = rotation_basis
+
+						parent_for_meshes.add_child(new_mesh_instance)
+
+	grid_map_node.queue_free() # Optional: Remove the GridMap after conversion
 	player = get_node(player_path)
 
 
