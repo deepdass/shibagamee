0e8ba402b742811ba8c5766af24a1ba79f89d9b6
deepdass <deepdas26052008@gmail.com>
2025-09-27 22:06:51 +0530

    4.5 shift and bmm

--- Scripts/2enemy/Rogue/skeleton_projectiles.gd
+++ Scripts/2enemy/Rogue/skeleton_projectiles.gd
@@ -15,5 +15,4 @@ func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("decrease_health"):
 		body.decrease_health()
-	else:
-		queue_free()
+	
