d110df98f87b56e77d2be331571b7557a5fa8d9b
deepdass <deepdas26052008@gmail.com>
2025-09-30 18:53:09 +0530

    new lib

--- Scripts/2enemy/Rogue/skeleton_projectiles.gd
+++ Scripts/2enemy/Rogue/skeleton_projectiles.gd
@@ -15,4 +15,5 @@ func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("decrease_health"):
 		body.decrease_health()
+		queue_free()
 	
