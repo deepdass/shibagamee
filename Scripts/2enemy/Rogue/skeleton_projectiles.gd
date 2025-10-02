b2598e9d142723c7721f7c7b0ba255a419b70c27
deepdass <deepdas26052008@gmail.com>
2025-10-02 14:33:04 +0530

    checkpt

--- Scripts/2enemy/Rogue/skeleton_projectiles.gd
+++ Scripts/2enemy/Rogue/skeleton_projectiles.gd
@@ -15,5 +15,6 @@ func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("decrease_health"):
 		body.decrease_health()
+	elif !body.has_method("take_damage"):
 		queue_free()
 	
