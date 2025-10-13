b02edfd175eb5a09f0379a54be8459ff769d1287
deepdass <deepdas26052008@gmail.com>
2025-10-13 22:25:10 +0530

    new auto attack logic

--- Scripts/0Managers/0_game_manager.gd
+++ Scripts/0Managers/0_game_manager.gd
@@ -110,7 +110,7 @@ func won() -> void:
 
 func _on_area_3d_body_entered(body: Node3D) -> void: ##inside crypt
 	if body == player:
-		get_tree().change_scene_to_file("res://Scenes/map/underWorldPro.tscn")
+		get_tree().change_scene_to_file("res://Scenes/map/underWorld.tscn")
 
 
 func _on_killzone_body_entered(body: Node3D) -> void:
