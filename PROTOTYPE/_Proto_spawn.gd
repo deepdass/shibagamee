dd1bc135745fcb660ae102697297fe1b358640b5
deepdass <deepdas26052008@gmail.com>
2025-10-25 23:55:18 +0530

    spawn fix

--- PROTOTYPE/_Proto_spawn.gd
+++ PROTOTYPE/_Proto_spawn.gd
@@ -5,6 +5,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().create_timer(.7).timeout
 	var thisscene : Node3D = whichscene.instantiate()
 	self.add_child(thisscene)
 	thisscene.global_position = self.global_position
