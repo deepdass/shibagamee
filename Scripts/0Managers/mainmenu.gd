70fd99da975e02805982f1acf059d63e5d2f6063
deepdass <deepdas26052008@gmail.com>
2025-11-02 20:37:38 +0530

    menu

--- Scripts/0Managers/mainmenu.gd
+++ Scripts/0Managers/mainmenu.gd
@@ -1,20 +1,26 @@
 extends Control
 
-var overworld : String = "res://Scenes/map/overWorld.tscn"
+const overworld : String = "res://Scenes/map/overWorld.tscn"
 
-var underworld : String = "res://Scenes/map/underWorld.tscn"
-var PDG_underworld : String = "res://Scenes/map/PDG_underWorld.tscn"
+const underworld : String = "res://Scenes/map/underWorld.tscn"
+const PDG_underworld : String = "res://Scenes/map/PDG_underWorld.tscn"
 
 @onready var play_panel: Panel = $"Play panel"
+@onready var options_panel: Panel = $"options panel"
 
-func _on_b_play_pressed() -> void:
-	play_panel.visible = true
 	
 func _on_b_quit_pressed() -> void:
 	get_tree().quit()
 
 
 #######################
+func _on_b_play_pressed() -> void:
+	options_panel.visible = false
+	if !play_panel.visible:
+		play_panel.visible = true
+	else:
+		play_panel.visible = false
+
 func _on_pdg_pressed() -> void:
 	Global.which_scene = PDG_underworld
 	get_tree().change_scene_to_file(overworld)
@@ -25,3 +31,17 @@ func _on_handcraft_pressed() -> void:
 
 func _on_back_pressed() -> void:
 	play_panel.visible = false
+	options_panel.visible = false
+
+#########################
+
+func _on_b_option_pressed() -> void:
+	play_panel.visible = false
+	if !options_panel.visible:
+		options_panel.visible = true
+	else:
+		options_panel.visible = false
+
+
+func _on_changelang_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/map/SelectLang.tscn")
